9b273352ad0d89414859f46d263225d7c6a2a1ec
jamaima-j <jamaimajan@gmail.com>
2024-07-07 13:22:38 -0500

    backend updated

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
@@ -9,15 +10,17 @@ app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/volunteer', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Use routes
-app.use('/events', require('./routes/events'));
+app.use('/admin/events', require('./routes/events'));
 
-// Start the server
+// Test route
+app.get('/test', (req, res) => {
+  res.send('Server is running');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
