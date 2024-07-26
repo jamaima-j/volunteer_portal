6d078ce149b14ff0917d646fb8cec3bdcf7bf62c
jamaima-j <jamaimajan@gmail.com>
2024-07-25 21:31:37 -0500

    database

--- server/index.js
+++ server/index.js
@@ -1,23 +1,14 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 const { v4: uuidv4 } = require('uuid');
-=======
-const dotenv = require('dotenv');
-
-dotenv.config();
->>>>>>> 1c8ca4bf1693161c343141d61e0432d0b9a72a09
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-<<<<<<< HEAD
 // MongoDB connection
 mongoose.connect('mongodb://localhost:27017/volunteer', {
   useNewUrlParser: true,
@@ -27,42 +18,19 @@ mongoose.connect('mongodb://localhost:27017/volunteer', {
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch(error => console.error('Could not connect to MongoDB:', error));
-=======
-// Print the MONGODB_URI to verify it's being read correctly
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => {
-    console.error('Could not connect to MongoDB', err);
-    process.exit(1);
-  });
->>>>>>> 1c8ca4bf1693161c343141d61e0432d0b9a72a09
 
 // Import routes
 const eventsRoute = require('./routes/events');
 const registrationRoute = require('./routes/registration');
 const loginRoute = require('./routes/login');
-<<<<<<< HEAD
 const profileRoute = require('./routes/profile');
 const notificationsRoute = require('./routes/notifications');
-=======
->>>>>>> 1c8ca4bf1693161c343141d61e0432d0b9a72a09
 
 // Use routes
 app.use('/admin/events', eventsRoute);
 app.use('/auth', registrationRoute);
 app.use('/auth', loginRoute);
-<<<<<<< HEAD
 app.use('/profile', profileRoute);
 app.use('/notifications', notificationsRoute);
-=======
-
-// Test route
-app.get('/test', (req, res) => {
-  res.send('Server is running');
-});
->>>>>>> 1c8ca4bf1693161c343141d61e0432d0b9a72a09
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
