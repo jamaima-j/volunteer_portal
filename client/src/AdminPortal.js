a94d92bca610be129e57fc3190756443d7d2dd05
leann-alkhateeb <leann_alkhateeb@hotmail.com>
2024-08-06 11:12:21 -0500

    reporting module

--- client/src/AdminPortal.js
+++ client/src/AdminPortal.js
@@ -100,6 +100,36 @@ const AdminPortal = () => {
       console.error('Error matching volunteer:', error);
     }
   };
+  
+  const downloadPDFReport = () => {
+    axios.get('http://localhost:5000/admin/reporting/report/pdf', { responseType: 'blob' })
+      .then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'volunteer_report.pdf');
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch(error => {
+        console.error('There was an error generating the PDF report!', error);
+      });
+  };
+
+  const downloadCSVReport = () => {
+    axios.get('http://localhost:5000/admin/reporting/report/csv', { responseType: 'blob' })
+      .then(response => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'volunteer_report.csv');
+        document.body.appendChild(link);
+        link.click();
+      })
+      .catch(error => {
+        console.error('There was an error generating the CSV report!', error);
+      });
+  };
 
   const openTab = (evt, tabName) => {
     const tabcontent = document.getElementsByClassName('tabcontent');
